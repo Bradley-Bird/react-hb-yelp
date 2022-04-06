0386bbda03a10e1f9983d7aea8b0c05ba35d9c1c
Bradley-Bird <Bradleybird2@icloud.com>
2022-04-06 14:50:32 -0700

    api connected and displaying name + rating

--- functions/fetch-yelp/index.js
+++ functions/fetch-yelp/index.js
@@ -4,6 +4,30 @@ require('dotenv').config({ path: `.env.development.local` });
 const handler = async (event) => {
   // add code here to fetch data from yelp API
   // be sure to include the parameters from event.queryStringParameters
+  const { zip } = event.queryStringParameters;
+  try {
+    const resp = await fetch(
+      `https://api.yelp.com/v3/businesses/search?location=${zip}&category="restaurants"`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.YELP_API_KEY}`,
+        },
+      }
+    );
+    const data = await resp.json();
+    const json = JSON.stringify(data);
+
+    return {
+      statusCode: 200,
+      body: json,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+  return {
+    statusCode: 500,
+    body: JSON.stringify({ error: 'Failed fetching data' }),
+  };
 };
 
 module.exports = { handler };
